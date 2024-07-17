29884373848d1d779b5fcbd647e2b9c286204962
Bhosale <mayur.b.bhosale@capgemini.com>
2024-07-17 14:27:39 +0530

    Checking Exception

--- src/test/java/com/cwm/ComparingArrays.java
+++ src/test/java/com/cwm/ComparingArrays.java
@@ -19,12 +19,32 @@ public class ComparingArrays {
 		int[] expected= {10,30,45,56,80};
 		
 //		Following method will comapre the memory addresses of given array
-//	java.lang.AssertionError: expected:<[I@5ef60048> but was:<[I@1d548a08>	
+//	java.lang.AssertionError: expected:<[I@5ef60048> but was:<[I@1d548a08>
+		
 //		assertEquals(expected,number);
 		
 		assertArrayEquals(expected, number);
 	}
 	
+	//Checking Exception
+	
+	@Test
+	public void testArrayNullArray() {
+//		By using try and catch Block
+		int[] numbers=null;
+		try {
+		Arrays.sort(numbers);
+		}
+		catch (NullPointerException e) {
+//			Success
+		}
+		
+	}
+	@Test(expected = NullPointerException.class)
+	public void testArrayNullPointerExceprion(){
+		int[] numbers=null;
+		Arrays.sort(numbers);
+	}
 	
 
 
